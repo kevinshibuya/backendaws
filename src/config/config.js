bc111b353fecf17c0ca3f943dec9e777264459ea
kevinshibuya <kevinshibuya@hotmail.com>
2020-10-10 14:59:10 -0300

    pronto para ec2

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,12 @@
 module.exports = {
     development: {
         database: {
-            host: 'ls-1e902954d5588c29189bea3a35415ab318d56d1a.ceefo25bexkd.us-east-1.rds.amazonaws.com',
+            host: 'ls-f161169bca164f762ac49b84b20f6a71cd0aaee7.chsrwzg7b2sf.us-east-1.rds.amazonaws.com',
             port: 3306,
             name: 'db_usuario',
             dialect: 'mysql',
             user: 'dbmasteruser',
-            password: 'g.UhB{l_).8n.e&7ZyAXWPT+`:6p|OWT'
+            password: '$vs40OE!D)iqJ&Z7=XK9w6Nm9T`4,S:G'
         }
     },
     production: {
